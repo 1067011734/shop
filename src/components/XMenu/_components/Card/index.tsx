3282af750716bced4d519b6e4522e19c61d6aa91
ChenLei <chen.lei@wowoohr.com>
2019-06-04 11:37:40 +0800

    fix: css

--- src/components/XMenu/_components/Card/index.tsx
+++ src/components/XMenu/_components/Card/index.tsx
@@ -29,7 +29,6 @@ class Index extends Component<CardProps> {
     const { dataSource, operation } = this.props
 
     return (
-      <View className={prefixCls}>
         <View className={`${prefixCls}`}>
           <View className={`${prefixCls}-header`}>
             <Image src={dataSource.src || "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1559141090669&di=dac14b86f6855b4f3453f03f773a6c3b&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201509%2F28%2F20150928012450_HzGKk.jpeg"} mode="aspectFit" />
@@ -41,20 +40,16 @@ class Index extends Component<CardProps> {
             <View className={`${prefixCls}-body-description`}>
               {dataSource.description}
             </View>
-            {/* <View className={`${prefixCls}-body-footer`}>
-              <View className={`${prefixCls}-body-footer-description`}>
-                {dataSource.description}
-              </View>
+            <View className={`${prefixCls}-body-footer`}>
               <View className={`${prefixCls}-body-footer-price`}>
                 ￥ {dataSource.price}
               </View>
-            </View> */}
+              <View className={`${prefixCls}-body-footer-plus`}>
+
+              </View>
+            </View>
           </View>
         </View>
-        {/* <View className={`${prefixCls}-footer`}>
-          <XButton>{operation}</XButton>
-        </View> */}
-      </View>
     )
   }
 }
