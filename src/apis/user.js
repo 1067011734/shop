c4f940dd91afff44f95b8597c4669dd9a507f452
ChenLei <chen.lei@wowoohr.com>
2019-06-11 14:30:16 +0800

    fix: 优化

--- src/apis/user.js
+++
@@ -1,9 +0,0 @@
-import { wrapRequest } from './_utils';
-
-export const getUserData = wrapRequest('getName', {
-  mock: true
-});
-
-export const getUserData2 = wrapRequest('getUser2', {
-  mock: false,
-});
